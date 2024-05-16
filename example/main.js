e2e5fa219c386e410c4470fbb373d4bc40a6ab75
juannitro17 <juannitro17@gmail.com>
2024-05-16 14:51:36 +0000

    primer ejemplo usando 10x

---
+++ example/main.js
@@ -0,0 +1,9 @@
+console.log("hola")
+console.log("bienvenidos a la materia de principios de programacion logica")
+
+/*
+pasos para mandar cambios a repositorio desde terminal
+1.-git add .
+2.-git commit -m "mensaje de lo que mandas al repo"
+3.-git push
+*/
